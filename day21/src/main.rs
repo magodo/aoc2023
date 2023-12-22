9c114b402334ab1d515c035ef26b12c3f8674f1e
magodo <wztdyl@sina.com>
2023-12-22 17:27:57 +0800

    day21 p2

--- day21/src/main.rs
+++ day21/src/main.rs
@@ -15,10 +15,20 @@ struct Point {
 }
 
 impl Point {
-    fn offset(&self, x_offset: isize, y_offset: isize) -> Point {
+    fn offset(
+        &self,
+        x_offset: isize,
+        y_offset: isize,
+        height: usize,
+        width: usize,
+    ) -> Option<Point> {
         let x = self.x + x_offset;
         let y = self.y + y_offset;
-        Point { x, y }
+        if !(0..height as isize).contains(&x) || !(0..width as isize).contains(&y) {
+            None
+        } else {
+            Some(Point { x, y })
+        }
     }
 }
 
@@ -73,32 +83,27 @@ impl Map {
         let mut new_wl = Vec::new();
         for p in self.wl.clone() {
             for offset in [(0, 1), (0, -1), (1, 0), (-1, 0)] {
-                let next_p = p.offset(offset.0, offset.1);
-                let next_tile = self.get(next_p);
-                if next_tile == Tile::Rock {
-                    continue;
+                let next_p = p.offset(offset.0, offset.1, self.height, self.width);
+                if let Some(next_p) = next_p {
+                    let next_tile = self.get(next_p);
+                    if next_tile == Tile::Rock {
+                        continue;
+                    }
+                    if self.reached.contains_key(&next_p) {
+                        continue;
+                    }
+                    self.reached.insert(next_p, step + 1);
+                    new_wl.push(next_p);
                 }
-                if self.reached.contains_key(&next_p) {
-                    continue;
-                }
-                self.reached.insert(next_p, step);
-                new_wl.push(next_p);
             }
         }
         self.wl = new_wl;
     }
 
-    fn walk(&mut self, step: u64) -> u64 {
+    fn walk(&mut self, step: u64) {
         for step in 0..step {
             self.walk_one_step(step);
         }
-        self.reached.iter().fold(0, |acc, (_, reach_step)| {
-            if reach_step % 2 == (step - 1) % 2 {
-                acc + 1
-            } else {
-                acc
-            }
-        })
     }
 }
 
@@ -109,5 +114,67 @@ fn main() {
     }
     let content = read_to_string(&args[1]).unwrap();
     let mut map = Map::new(&content);
-    println!("{}", map.walk(26501365));
+
+    let step = 64;
+    map.walk(step);
+    let p1 = map.reached.iter().fold(0, |acc, (_, reach_step)| {
+        if reach_step % 2 == (step) % 2 {
+            acc + 1
+        } else {
+            acc
+        }
+    });
+    println!("{}", p1);
+
+    let mut map = Map::new(&content);
+    let step = 131;
+    map.walk(step);
+    let odd_full = map.reached.iter().fold(
+        0,
+        |acc, (_, reach_step)| {
+            if reach_step % 2 == 1 {
+                acc + 1
+            } else {
+                acc
+            }
+        },
+    );
+    let even_full = map.reached.iter().fold(
+        0,
+        |acc, (_, reach_step)| {
+            if reach_step % 2 == 0 {
+                acc + 1
+            } else {
+                acc
+            }
+        },
+    );
+    let odd_corners = map.reached.iter().fold(0, |acc, (_, reach_step)| {
+        if reach_step % 2 == 1 && *reach_step > 65 {
+            acc + 1
+        } else {
+            acc
+        }
+    });
+    let even_corners = map.reached.iter().fold(0, |acc, (_, reach_step)| {
+        if reach_step % 2 == 0 && *reach_step > 65 {
+            acc + 1
+        } else {
+            acc
+        }
+    });
+    let n = ((26501365 - (131 / 2)) / 131) as usize;
+    assert_eq!(n, 202300);
+    let p2 = ((n + 1) * (n + 1)) * odd_full + (n * n) * even_full - (n + 1) * odd_corners
+        + n * even_corners;
+
+    println!("{}", p2);
+
+    // println!(
+    //     "{:?}",
+    //     map.reached
+    //         .iter()
+    //         .filter(|r| *r.1 == 130)
+    //         .collect::<Vec<_>>()
+    // );
 }
