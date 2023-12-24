051b7d9deaa42817705cad589509dfb0f7ffebdf
magodo <wztdyl@sina.com>
2023-12-24 21:17:55 +0800

    day23

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,4 +1,5 @@
-use std::collections::HashSet;
+use std::cmp::max;
+use std::collections::{HashMap, HashSet};
 use std::env;
 use std::fs::read_to_string;
 
@@ -29,7 +30,7 @@ impl Point {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Direction {
     U,
     D,
@@ -37,7 +38,7 @@ enum Direction {
     R,
 }
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 enum Tile {
     Path,
     Forest,
@@ -49,14 +50,8 @@ struct Map {
     tiles: Vec<Tile>,
     width: usize,
     height: usize,
-}
-
-#[derive(Debug)]
-struct Hike<'a> {
-    map: &'a Map,
-    pos: Point,
+    start: Point,
     end: Point,
-    reached: HashSet<Point>,
 }
 
 impl Map {
@@ -79,6 +74,11 @@ impl Map {
             tiles,
             width,
             height,
+            start: Point { x: 0, y: 1 },
+            end: Point {
+                x: height - 1,
+                y: width - 2,
+            },
         }
     }
 
@@ -86,79 +86,163 @@ impl Map {
         self.tiles[p.x * self.width + p.y]
     }
 
-    fn all_hikes(&self) -> Vec<Hike> {
-        let mut hikes = vec![Hike::new(
-            Point { x: 0, y: 1 },
-            Point {
-                x: self.height - 1,
-                y: self.width - 2,
-            },
-            &self,
-        )];
+    fn neighbours(&self, point: Point, ignore_slope: bool) -> Vec<Point> {
+        let offsets: Vec<_> = match &self.get(point) {
+            Tile::Path => vec![(-1, 0), (1, 0), (0, -1), (0, 1)],
+            Tile::Forest => vec![],
+            Tile::Slope(dir) => {
+                if ignore_slope {
+                    vec![(-1, 0), (1, 0), (0, -1), (0, 1)]
+                } else {
+                    vec![match dir {
+                        Direction::U => (-1, 0),
+                        Direction::D => (1, 0),
+                        Direction::L => (0, -1),
+                        Direction::R => (0, 1),
+                    }]
+                }
+            }
+        };
 
-        while !hikes.iter().all(|hike| hike.is_end()) {
-            hikes = hikes.into_iter().flat_map(|hike| hike.next()).collect();
+        let mut neighbours = Vec::new();
+        for offset in offsets {
+            if let Some(p) = point.offset(offset.0, offset.1, self.height, self.width) {
+                if self.get(p) != Tile::Forest {
+                    neighbours.push(p);
+                }
+            }
         }
-
-        hikes
+        neighbours
     }
 }
 
-impl<'a> Hike<'a> {
-    fn new(start: Point, end: Point, map: &'a Map) -> Hike {
-        Hike {
-            map,
-            pos: start,
-            end,
-            reached: HashSet::from([start]),
+#[derive(Debug)]
+struct Graph {
+    start: Point,
+    end: Point,
+    edges: HashMap<Point, HashMap<Point, usize>>,
+}
+
+impl Graph {
+    fn new(map: &Map, ignore_slope: bool) -> Self {
+        let mut nodes = HashMap::from([
+            (
+                map.start,
+                vec![Point {
+                    x: map.start.x + 1,
+                    y: map.start.y,
+                }],
+            ),
+            (
+                map.end,
+                vec![Point {
+                    x: map.end.x - 1,
+                    y: map.end.y,
+                }],
+            ),
+        ]);
+        map.tiles.iter().enumerate().for_each(|(idx, t)| {
+            if *t == Tile::Forest {
+                return;
+            }
+            let pos = Point {
+                x: idx / map.width,
+                y: idx % map.width,
+            };
+            let neighbours = map.neighbours(pos, ignore_slope);
+            if neighbours.len() >= 3 {
+                nodes.insert(pos, neighbours);
+            }
+        });
+
+        let mut edges = HashMap::new();
+        let targets: HashSet<_> = nodes.keys().map(|e| e.clone()).collect();
+        nodes.iter().for_each(|(node, neighbours)| {
+            for np in neighbours {
+                let mut hike = SingleHike::new(*np, HashSet::from([*np, *node]), map, &targets);
+                let walk_res = hike.walk(ignore_slope);
+                if let Some(peer) = walk_res {
+                    let edge = edges.entry(*node).or_insert(HashMap::new());
+                    assert!(!edge.contains_key(&peer));
+                    edge.insert(peer, hike.reached.len());
+                }
+            }
+        });
+        Self {
+            start: map.start,
+            end: map.end,
+            edges,
         }
     }
 
-    fn next(self) -> Vec<Self> {
-        if self.is_end() {
-            return vec![self];
+    fn max_step(&self) -> usize {
+        let step = self.dfs(self.start, HashSet::from([self.start]));
+        step as usize
+    }
+
+    fn dfs(&self, point: Point, seen: HashSet<Point>) -> isize {
+        if point == self.end {
+            return 0;
         }
-        let mut next_steps = Vec::new();
-        let offsets: Vec<_> = match &self.map.get(self.pos) {
-            Tile::Path => vec![(-1, 0), (1, 0), (0, -1), (0, 1)],
-
-            Tile::Forest => vec![],
-            Tile::Slope(dir) => vec![match dir {
-                Direction::U => (-1, 0),
-                Direction::D => (1, 0),
-                Direction::L => (0, -1),
-                Direction::R => (0, 1),
-            }],
-        };
-
-        for offset in offsets {
-            if let Some(p) = self
-                .pos
-                .offset(offset.0, offset.1, self.map.height, self.map.width)
-            {
-                next_steps.push(p);
-            }
-        }
-
-        let mut hikes = Vec::new();
-        for np in next_steps {
-            if self.reached.contains(&np) {
+        let edges = &self.edges[&point];
+        let mut step = isize::MIN;
+        for (next_point, d) in edges {
+            if seen.contains(&next_point) {
                 continue;
             }
-            let mut reached = self.reached.clone();
-            reached.insert(np);
-            hikes.push(Hike {
-                pos: np,
-                reached,
-                ..self
-            });
+            let mut seen = seen.clone();
+            seen.insert(*next_point);
+            let follow_step = self.dfs(*next_point, seen);
+            if (*d as isize + follow_step) > step {
+                step = max(step, *d as isize + follow_step);
+            }
         }
+        step
+    }
+}
 
-        hikes
+#[derive(Debug)]
+struct SingleHike<'a> {
+    map: &'a Map,
+    pos: Point,
+    targets: &'a HashSet<Point>,
+    reached: HashSet<Point>,
+}
+
+impl<'a> SingleHike<'a> {
+    fn new(
+        start: Point,
+        reached: HashSet<Point>,
+        map: &'a Map,
+        targets: &'a HashSet<Point>,
+    ) -> Self {
+        SingleHike {
+            map,
+            pos: start,
+            targets,
+            reached,
+        }
     }
 
-    fn is_end(&self) -> bool {
-        self.pos == self.end
+    fn walk(&mut self, ignore_slope: bool) -> Option<Point> {
+        loop {
+            let neighbours: Vec<_> = self
+                .map
+                .neighbours(self.pos, ignore_slope)
+                .into_iter()
+                .filter(|p| !self.reached.contains(p))
+                .collect();
+            if neighbours.len() == 0 {
+                return None;
+            }
+            assert!(neighbours.len() == 1);
+            let np = neighbours[0];
+            if self.targets.contains(&np) {
+                return Some(np);
+            }
+            self.pos = np;
+            self.reached.insert(np);
+        }
     }
 }
 
@@ -168,8 +252,12 @@ fn main() {
         panic!("./exe <file>");
     }
     let content = read_to_string(&args[1]).unwrap();
+
     let map = Map::new(&content);
-    let hikes = map.all_hikes();
-    let hike_steps: Vec<_> = hikes.iter().map(|hike| hike.reached.len() - 1).collect();
-    println!("{:?}", hike_steps.iter().max().unwrap());
+
+    let graph = Graph::new(&map, false);
+    println!("{}", graph.max_step());
+
+    let graph = Graph::new(&map, true);
+    println!("{}", graph.max_step());
 }
